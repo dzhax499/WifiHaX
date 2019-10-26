8a5224a643a65d755c94b0eaaf1bb0b4fa969797
dzhax499 <57004084+dzhax499@users.noreply.github.com>
2019-10-27 06:03:30 +0700

    Add files via upload

---
+++ wifihax.sh
@@ -0,0 +1,110 @@
+clear
+echo "
+        =========================
+           WIFI HAX NO ROOT!
+        =========================
+          By      : Mr.DzHax
+          youtube : YtDzakir
+          IG/FB   : Dzakirtsabit
+        ========================="
+sleep 3
+read -p "Siapa nick kaka? " nama;
+echo "                  "
+echo "   SCANNING....    "
+sleep 3
+ifconfig
+ssh -R 1220:127.0.0.1:1220 > serveo.net
+sleep 3
+ifconfig
+ifconfig
+ifconfig
+ifconfig
+ifconfig
+ping -s 1000
+ping www.google.com
+sleep 3
+ping www.alhax.com
+sleep 5
+echo "DONE!"
+sleep 3
+clear
+echo "================================================"
+echo "[[[[[[[[[[=== Wifi Hax No Root ^_^ ===]]]]]]]]]]"
+echo "================================================"
+sleep 3
+echo "================================================"
+echo "  Yazid "
+echo "================================================"
+sleep 0.05
+echo "------------------------------------------------"
+echo "  RIZQINURQOLBI "
+echo "------------------------------------------------"
+sleep 0.05
+echo "::::::::::::::::::::::::::::::::::::::::::::::::"
+echo "  RW 23 (Full) "
+echo "::::::::::::::::::::::::::::::::::::::::::::::::"
+sleep 0.05
+echo "================================================="
+echo "  DzHax "
+echo "================================================="
+sleep 0.05
+read -p "Hack or Crack password h/c? ===> " hack;
+if [ $hack = "h" ];
+then
+    echo " HACKING INDONESIAN.... "
+    sleep 10
+    echo " CYBER DZHAX... "
+    sleep 15
+    echo " preparing 2% "
+    sleep 5
+    echo " preparing 5% "
+    sleep 3
+    echo " preparing 10% "
+    echo " preparing 15% "
+    sleep 0.05
+    echo " preparing 22% "
+    sleep 2
+    echo " preparing 26% "
+    sleep 2
+    echo " preparing 31% "
+    sleep 4
+    echo " preparing 40% "
+    sleep 2
+    echo " preparing 51% "
+    echo " preparing 60% "
+    sleep 5
+    echo " preparing 79% "
+    sleep 2
+    echo " preparing 89% "
+    sleep 2
+    echo " preparing 98% "
+    sleep 4
+    echo " preparing 100% "
+    sleep 5
+    echo "Finishing...."
+    sleep 9
+    echo " Jangan Lupa Subscribe Channel YtDzakir "
+    sleep 20
+    echo " Mengalihkan.... "
+    sleep 5
+    echo "
+           =============================
+           NAMA          ///// PASSWORD
+           =============================
+           Yazid         ///// yazid 123
+           RIZQINURQOLBI ///// RIZQINURQOLBI
+           RW-23        /////  TOPASRAYA
+           DzHax        /////  HackerComunity
+           REFAH       //////  hdi2db2uhd3y72dboobqce??/qid828=[2[[d]29dh3d829hdqweq
+           ============================= "
+    echo " Jika Ada yang enggak ngerti pass nya berarti enggak bisa coi!!"
+    sleep 3
+    echo " Enjoy Your Free WIFI ^_^ ! "
+    sleep 4
+elif [ $hack = "c" ];
+then
+    echo "SOON!"
+    sleep 3
+    echo "Sedang ada masalah!"
+    sleep 3
+fi
